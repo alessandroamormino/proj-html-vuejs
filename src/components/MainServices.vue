105438638187715a7c43f007ae0562cf6206bb8e
alessandroamormino <amormino.alessandro97@gmail.com>
2023-04-11 17:14:00 +0200

    feat: add MainServices component

--- src/components/MainServices.vue
+++ src/components/MainServices.vue
@@ -2,7 +2,34 @@
 export default {
   name: 'MainServices',
   data() {
-    return {}
+    return {
+      services: [
+        {
+          title: 'Rehabilitation Center',
+          img: '/images/icon-14.png'
+        },
+        {
+          title: 'Dental Implants',
+          img: '/images/icon-15.png'
+        },
+        {
+          title: 'Medicine Research',
+          img: '/images/icon-16.png'
+        },
+        {
+          title: 'Bload Bank',
+          img: '/images/icon-11.png'
+        },
+        {
+          title: 'Pharmaceutical Advice',
+          img: '/images/icon-12.png'
+        },
+        {
+          title: 'Medical Counseling',
+          img: '/images/icon-13.png'
+        },
+      ],
+    }
   }
 }
 </script>
@@ -16,6 +43,14 @@ export default {
       <div class="separator"></div>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est aliquam labore consequatur similique ipsam cum.
         Maiores velit sint autem, nulla consequuntur obcaecati eaque ea totam itaque quaerat mollitia ab!</p>
+    </div>
+    <div class="container-centered">
+      <div v-for="service in this.services" class="card">
+        <img :src="service.img" :alt="service.title">
+        <span id="title">{{ service.title }}</span>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem beatae saepe rerum molestiae amet?
+          Quisquam quod saepe, mollitia sunt voluptatem!</p>
+      </div>
     </div>
   </div>
 </template>
@@ -52,6 +87,45 @@ export default {
       width: 150px;
       height: 1px;
       background-color: #80808080;
+    }
+  }
+
+  .container-centered {
+    display: flex;
+    flex-flow: row wrap;
+    gap: 2em;
+    // flex
+
+    padding-top: 80px;
+
+    .card {
+      @include flexColumn();
+      // flex
+      padding-bottom: 60px;
+      text-align: center;
+      width: calc(100% / 3 - 2em / 3 * 2);
+
+      img {
+        width: 70px;
+      }
+
+      #title {
+        text-transform: uppercase;
+        padding: .5em 0;
+      }
+
+      p {
+        font-size: .9em;
+      }
+
+      &:hover {
+        cursor: pointer;
+      }
+
+      &:hover #title {
+        color: $colorPrimary;
+      }
+
     }
   }
 }
