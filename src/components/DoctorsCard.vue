01fa3f0255ce97b0e4847ca91ae8f58ff6adc56b
alessandroamormino <amormino.alessandro97@gmail.com>
2023-04-11 16:16:21 +0200

    feat: import prop in DoctorsCard component

--- src/components/DoctorsCard.vue
+++ src/components/DoctorsCard.vue
@@ -1,5 +1,6 @@
 <script>
 export default {
+  name: 'DoctorsCard',
   data() {
     return {}
   },
@@ -8,5 +9,77 @@ export default {
   }
 }
 </script>
-<template></template>
-<style lang="scss" scoped></style>
+<template>
+  <div class="card">
+    <div class="top">
+      <img :src="doctorCard.img" :alt="doctorCard.name">
+    </div>
+    <div class="bottom">
+      <span id="name">{{ doctorCard.name }}</span>
+      <span id="role">{{ doctorCard.role }}</span>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit suscipit vel illum sunt.</p>
+      <div class="socials">
+        <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
+        <a href="#"><i class="fa-brands fa-square-twitter"></i></a>
+        <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.card {
+
+  .top {
+    width: 300px;
+    height: 300px;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      display: inline-block;
+    }
+  }
+
+  .bottom {
+    display: flex;
+    flex-flow: column nowrap;
+    // flex
+
+    width: 300px;
+    height: 300px;
+    padding: 2em;
+    background-color: #fff;
+
+    #name {
+      text-transform: uppercase;
+    }
+
+    p {
+      font-size: .9em;
+      padding: 1.5em 0;
+    }
+
+    .socials {
+      display: flex;
+      gap: .5em;
+
+      .fa-brands {
+        font-size: 2em;
+      }
+
+      .fa-square-facebook {
+        color: #36599C;
+      }
+
+      .fa-square-twitter {
+        color: #30AEF3;
+      }
+
+      .fa-square-instagram {
+        color: #2F739E;
+      }
+    }
+  }
+}
+</style>
