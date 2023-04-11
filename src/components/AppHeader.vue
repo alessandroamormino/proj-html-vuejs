9769c4288123c622994ec98debe70c6826de1093
alessandroamormino <amormino.alessandro97@gmail.com>
2023-04-11 12:55:55 +0200

    style: header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,7 +4,6 @@ export default {
   name: 'AppHeader',
   data() {
     return {
-      title: 'Caring For Life',
       paragraph: 'Kind words can be short and easy to speak, but their echoes are truly endless. Avada Health focuses on you as if it was our own family.',
     };
   },
@@ -17,15 +16,49 @@ export default {
   <header>
     <HeaderNav></HeaderNav>
     <div class="container-centered">
-      <h1>{{ this.title }}</h1>
-      <p>{{ this.paragraph }}</p>
-      <button class="btn btn-primary">Learn More</button>
+      <div class="header-info">
+        <h1>
+          Caring <span class="life">For Life</span>
+        </h1>
+        <p>{{ this.paragraph }}</p>
+        <button class="btn btn-primary">Learn More</button>
+      </div>
     </div>
   </header>
 </template>
 <style lang="scss" scoped>
+@use '../scss/variables.scss' as *;
+
 header {
   background-image: url('/images/header-image-homepage.jpg');
+  background-position: center top;
   height: 850px;
+  color: white;
+
+  .container-centered {
+    height: 100%;
+    display: flex;
+    align-items: center;
+
+    .header-info {
+      display: flex;
+      flex-flow: column nowrap;
+      align-items: start;
+      gap: 1.5em;
+
+      width: 550px;
+
+      h1 {
+        font-weight: 100;
+        font-size: 3em;
+
+        .life {
+          color: $colorPrimary;
+        }
+      }
+    }
+  }
+
+
 }
 </style>
--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -44,7 +44,12 @@ export default {
 
 
 nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
   height: 100px;
+  // position
   padding: 0 5em;
   @include flexRowBetween();
 
