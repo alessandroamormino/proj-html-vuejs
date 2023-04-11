7373ec6b7ef76df5d5ea98103d12088830205698
alessandroamormino <amormino.alessandro97@gmail.com>
2023-04-11 16:16:30 +0200

    style: Doctors card

--- src/components/MainDoctors.vue
+++ src/components/MainDoctors.vue
@@ -1,4 +1,5 @@
 <script>
+import DoctorsCard from '../components/DoctorsCard.vue';
 export default {
   name: 'MainDoctors',
   data() {
@@ -6,21 +7,24 @@ export default {
       doctors: [
         {
           name: 'Jon Snow',
-          img: '/images/doctor-1-200x206.jpg',
+          img: '/images/doctor-1.jpg',
           role: 'Anesthesiologist'
         },
         {
-          name: 'Jon Snow',
-          img: '/images/doctor-1-200x206.jpg',
-          role: 'Anesthesiologist'
+          name: 'Tony Stark',
+          img: '/images/doctor-2.jpg',
+          role: 'Cardiologist'
         },
         {
-          name: 'Jon Snow',
-          img: '/images/doctor-1-200x206.jpg',
-          role: 'Anesthesiologist'
+          name: 'Anna Smith',
+          img: '/images/doctor-3.jpg',
+          role: 'Nurse Practitioner'
         },
       ],
     }
+  },
+  components: {
+    DoctorsCard
   }
 }
 </script>
@@ -35,8 +39,8 @@ export default {
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est aliquam labore consequatur similique ipsam cum.
         Maiores velit sint autem, nulla consequuntur obcaecati eaque ea totam itaque quaerat mollitia ab!</p>
     </div>
-    <div v-for="doctor in this.doctors" class="card">
-
+    <div class="card-container">
+      <DoctorsCard v-for="doctor in this.doctors" :doctorCard="doctor"></DoctorsCard>
     </div>
   </div>
 </template>
@@ -77,6 +81,15 @@ export default {
       height: 1px;
       background-color: #80808080;
     }
+  }
+
+  .card-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 2em;
+    // flex
+    padding: 3em 0;
   }
 }
 </style>
